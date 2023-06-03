72b616d25b4f95dbb63ae41d9db49bcc8029356b
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-06-03 16:22:42 +0530

    user model added

---
+++ models/Users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: String,
+    firstName: String,
+    lastName: String,
+    age: Number,
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', schema);
+
+module.exports = User;
